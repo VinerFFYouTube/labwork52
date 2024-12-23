115ff11aa66aa2fe3ffb115edd9dbb42d526396a
Elzar <psixyt14@gmail.com>
2024-12-23 18:48:06 +0600

    сделал уровень 1

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './components/Card/Card'
+
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
     <>
+      <Card rank="k" suit="♦"/>
     </>
   )
 }
