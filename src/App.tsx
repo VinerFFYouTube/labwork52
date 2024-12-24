26878c405508ce605b38ab4723e36aeb5a0747f3
Elzar <psixyt14@gmail.com>
2024-12-24 17:28:21 +0600

    сделал уровень 4

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,43 @@
-
-import { useState } from 'react'
-import './App.css'
-import Card from './components/Card/Card'
-import CardDesc from './lib/CardDesc'
-
+import { useState } from 'react';
+import './App.css';
+import Card from './components/Card/Card';
+import CardDesc from './lib/CardDesc';
+import PokerHand from './lib/PokerHand';
 
 const App = () => {
-  const [cards, setCards] = useState<any[]>()
+  const [cards, setCards] = useState<any[]>([]);
+
+  const a = new PokerHand(cards);
+  // a.af()
 
   const clickCard = () => {
-    const deck = new CardDesc();
-    const get5Cards = deck.getCards(5);
-    setCards(get5Cards)
-  }
 
-  const examinationCard = () => {
-    if (cards?.length != undefined) {
-      return (
-        <div className="playingCards faceImages">
-          {cards?.map((card) => (
-            <Card rank={card.rank} suit={card.suit} />
-          ))}
-        </div>
-      )
-    }
-  }
+
+
+    const deck = new CardDesc();
+    const get5Cards: any = deck.getCards(5);
+    setCards(get5Cards);
+
+    // console.log(cards[0].rank)
+
+  };
 
   return (
     <>
-      <button onClick={clickCard}>раздать карты</button>
-      {examinationCard()}
-    </>
-  )
-}
+      <button onClick={clickCard}>Раздать карты</button>
 
-export default App
+      {cards.length > 0 && (
+        <div>
+          <div className="playingCards faceImages">
+            {cards.map((card, index) => (
+              <Card key={index} rank={card.rank} suit={card.suit} />
+            ))}
+          </div>
+          <p>combination: {a.af()}</p>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default App;
--- src/lib/PokerHand.ts
+++ src/lib/PokerHand.ts
@@ -1,7 +1,50 @@
+interface typeCard {
+    rank: string;
+    suit: string;
+}
 class PokerHand {
+    private cardArrays: typeCard[];
+    constructor(cardArray: typeCard[]) {
+        this.cardArrays = cardArray;
+    }
+    af() {
+        if(this.cardArrays.length > 0) {
+            let cardsValue1: string[] = [`${this.cardArrays[0].rank}`, `${this.cardArrays[0].suit}`];
+            let cardsValue2: string[] = [`${this.cardArrays[1].rank}`, `${this.cardArrays[1].suit}`];
+            let cardsValue3: string[] = [`${this.cardArrays[2].rank}`, `${this.cardArrays[2].suit}`];
+            let cardsValue4: string[] = [`${this.cardArrays[3].rank}`, `${this.cardArrays[3].suit}`];
+            let cardsValue5: string[] = [`${this.cardArrays[4].rank}`, `${this.cardArrays[4].suit}`];
 
-    
-    constructor(rank: string; suit: string) {
+            if(cardsValue1[0] === 'A' && cardsValue2[0] === 'K' && cardsValue3[0] === 'Q' && cardsValue4[0] === 'J' && cardsValue5[0] === '10') {
+                if(cardsValue1[1] === cardsValue2[1] && cardsValue1[1] === cardsValue3[1] && cardsValue1[1] === cardsValue4[1] && cardsValue1[1] === cardsValue5[1]) {
+                    return 'royal flush'
+                }
+            } else if (+cardsValue1[0] > +cardsValue2[0] && +cardsValue2[0] > +cardsValue3[0] && +cardsValue3[0] > +cardsValue4[0] && +cardsValue4[0] > +cardsValue5[0]) {
+                if(cardsValue1[0] === cardsValue2[0] && cardsValue1[0] === cardsValue3[0] && cardsValue1[0] === cardsValue4[0] && cardsValue1[0] === cardsValue5[0]) {
+                    return 'straight flush'
+                }
+            } else if(cardsValue1[0] === cardsValue2[0] && cardsValue1[0] === cardsValue3[0] && cardsValue1[0] === cardsValue4[0] && cardsValue1[0] === cardsValue5[0]) {
+                return 'four of a kind'
+            } else if(cardsValue1[0] === cardsValue2[0] && cardsValue1[0] === cardsValue3[0] && cardsValue4[0] === cardsValue5[0]) {
+                return 'full house'
+            } else if(cardsValue1[1] === cardsValue2[1] && cardsValue1[1] === cardsValue3[1] && cardsValue1[1] === cardsValue4[1] && cardsValue1[1] === cardsValue5[1]) {
+                return 'flush'
+            } else if(+cardsValue1[0] > +cardsValue2[0] && +cardsValue2[0] > +cardsValue3[0] && +cardsValue3[0] > +cardsValue4[0] || +cardsValue2[0] > +cardsValue3[0] && +cardsValue3[0] > +cardsValue4[0] && +cardsValue4[0] > +cardsValue5[0]) {
+                return 'straight'
+            } else if(cardsValue1[0] === cardsValue2[0] && cardsValue1[0] === cardsValue3[0] || cardsValue3[0] === cardsValue4[0] && cardsValue3[0] === cardsValue5[0]) {
+                return 'three of a kind'
+            } else if(cardsValue1[0] === cardsValue2[0] && cardsValue3[0] === cardsValue4[0] || cardsValue2[0] === cardsValue3[0] && cardsValue4[0] === cardsValue5[0]) {
+                return 'two pairs'
+            } else if(cardsValue1[0] === cardsValue2[0] || cardsValue2[0] === cardsValue3[0] || cardsValue3[0] === cardsValue4[0] || cardsValue4[0] === cardsValue5[0]) {
+                return 'one pair'
+            } else {
+                return 'пока комбинаций нет'
+            }
 
+        } else {
+            console.log('карт пока нет')
+        }
     }
 }
+
+export default PokerHand
